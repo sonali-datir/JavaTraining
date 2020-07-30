79d520c94d3570d07c9c8dc098d728506dc784b2
Sonali Datir <34915163+sonali-datir@users.noreply.github.com>
2020-07-30 12:11:09 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 
 public class Calculator{
 	public static void main(String[] args){
-		int num1,num2,addition;
-        Scanner sc = new Scanner(System.in);
+	 int num1,num2,addition;
+         Scanner sc = new Scanner(System.in);
         
-		System.out.println("Enter First Number");
-        num1 = sc.nextInt();
+	 System.out.println("Enter First Number");
+         num1 = sc.nextInt();
         
-		System.out.println("Enter Second Number");
-        num2 = sc.nextInt();
+	 System.out.println("Enter Second Number");
+         num2 = sc.nextInt();
         
-		addition=num1+num2;
+	 addition=num1+num2;
         
-		System.out.println("Addition = "+addition);
+	 System.out.println("Addition = "+addition);
 	}
 }
