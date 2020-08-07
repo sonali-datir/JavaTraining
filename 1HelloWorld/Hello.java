b0fe1e410f65624ade8432154a2abb32835f1a6e
Sonali Datir <34915163+sonali-datir@users.noreply.github.com>
2020-08-07 17:42:49 +0530

    Delete Hello.java

--- 1HelloWorld/Hello.java
+++
@@ -1,9 +0,0 @@
-//Day 1 – Hello world. Just to verify that Java has been
-//setup well and we are off and running.
-
-public class Hello
-{
-	public static void main(String[] args){
-		System.out.println("Hello World!!");
-	}
-}
